c7ab54e9a062e3e1f26d93b6e42b210f34a83d23
hzg <7865702+he_zhiguo@user.noreply.gitee.com>
2020-08-03 18:04:27 +0800

    redis工具类

---
+++ redis-test/src/main/java/com/windrain/redistest/util/Status.java
@@ -0,0 +1,24 @@
+package com.windrain.redistest.util;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class Status {
+    public static enum ExpireEnum{
+        UNREAD_MSG(30L, TimeUnit.DAYS);
+
+        private Long time;
+        private TimeUnit timeUnit;
+        ExpireEnum(Long time,TimeUnit timeUnit){
+            this.time=time;
+            this.timeUnit=timeUnit;
+        }
+
+        public Long getTime(){
+            return time;
+        }
+
+        public TimeUnit getTimeUnit(){
+            return timeUnit;
+        }
+    }
+}
